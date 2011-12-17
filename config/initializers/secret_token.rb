1498ee604ebc3ee12ca9b534f3f5063bcbc3abf7
Schuyler Thompson <schuyler@sdt16-desktop.case.edu>
2011-12-17 14:07:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bio::Application.config.secret_token = '6aaf64a7730590facae0958149398a7f48ed44cf91bdb231ef10637e9f9c616aafbdfb095a94c38820d84e1504a8a4207e37f86793a38a7ce4c08b8c36604b42'
