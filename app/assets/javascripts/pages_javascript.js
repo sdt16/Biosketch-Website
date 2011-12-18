fe8ca4c8bdba54f0cf7485348d77b35b8cc687d1
Schuyler Thompson <skooz@skooz.net>
2011-12-17 22:22:28 -0500

    forgot to add js file

---
+++ app/assets/javascripts/pages_javascript.js
@@ -0,0 +1,15 @@
+
+$(window).ready(function(){
+    (function($){
+        $.fn.extend({
+            center: function () {
+                return this.each(function() {
+                    var left = ($(window).width() - $(this).outerWidth()) / 2;
+                    $(this).css({position:'absolute', margin:0, left: (left > 0 ? left : 0)+'px'});
+                });
+            }
+        });
+    })(jQuery);
+
+    $('#links').center();
+});
