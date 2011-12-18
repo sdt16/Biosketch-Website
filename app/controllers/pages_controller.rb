aa043bc4d63f94d426dbc8cf7347f77273381002
Schuyler Thompson <skooz@skooz.net>
2011-12-17 19:01:35 -0500

    start using compass

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,9 @@ require 'rdiscount'
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    file = IO.read("public/home.md")
+    markdown = Markdown.new(file)
+    @content = markdown.to_html
   end
 
   def bio
