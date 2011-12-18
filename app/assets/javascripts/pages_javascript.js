bbdde3d3185fb4262645180c8c4acf29ba3269ff
Schuyler Thompson <skooz@skooz.net>
2011-12-18 09:42:47 -0500

    still trying to fix this issue. I think it has to do with the js being collapsed

--- app/assets/javascripts/pages_javascript.js
+++ app/assets/javascripts/pages_javascript.js
@@ -1,13 +1,5 @@
-$(window).load(function(){
-    (function($){
-        $.fn.extend({
-            center: function () {
-                return this.each(function() {
-                    var left = ($(window).width() - $(this).outerWidth()) / 2;
-                    $(this).css({position:'absolute', margin:0, left: (left > 0 ? left : 0)+'px'});
-                });
-            }
-        });
-    })(jQuery);
-    $('#navlist').center();
+$('#navlist').position({
+    my: 'center',
+    at: 'center',
+    of: $('#links')
 });
